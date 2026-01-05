4d176248e64bbbd45264654c448210a70346f870
Nirman Patel <144795287+NirmanPatel036@users.noreply.github.com>
2026-01-05 15:02:40 +0530

    configure ESLint to ignore during builds

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ const __dirname = dirname(__filename)
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: { 
+    ignoreDuringBuilds: true,
+  }
   typescript: {
     ignoreBuildErrors: true,
   },
